cb552feac2d1621c7c871ce5f07403394eaf99ae
Zendau <cowalyow2012@gmail.com>
2025-03-20 22:01:57 +0300

    feat(config): add environment configuration loader with dev/prod validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `config/env/.env.${process.env.NODE_ENV || 'dev'}`, // Новый путь
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().hostname().required(),
+        DB_PORT: Joi.number().port().required(),
+        DB_USER: Joi.string().required(),
+        DB_PASS: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_SYNC: Joi.boolean().truthy('true').falsy('false').default(false),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
