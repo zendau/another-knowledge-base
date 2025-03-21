4c51f7ca1bfd001201b9411e6776919393c8b08f
Zendau <cowalyow2012@gmail.com>
2025-03-21 14:23:21 +0300

    feat(entities): add Article and Tag database entities

--- src/articles/entities/article.entity.ts
+++ src/articles/entities/article.entity.ts
@@ -1,1 +1,40 @@
-export class Article {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  // ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToMany,
+  JoinTable,
+} from 'typeorm';
+// import { User } from './user.entity';
+import { Tag } from './tag.entity';
+
+@Entity('articles')
+export class Article {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('text')
+  content: string;
+
+  @Column({ default: true })
+  isPublic: boolean;
+
+  // @ManyToOne(() => User, (user) => user.articles, { onDelete: 'CASCADE' })
+  // author: User;
+
+  @ManyToMany(() => Tag, (tag) => tag.articles, { cascade: true })
+  @JoinTable({ name: 'article_tags' })
+  tags: Tag[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
