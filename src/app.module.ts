5ccabdce55fde28930279b2bcdd780b4e4bd04ef
Zendau <cowalyow2012@gmail.com>
2025-03-21 14:39:40 +0300

    feat(user): initialize user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticlesModule } from './articles/articles.module';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import * as Joi from 'joi';
 
 @Module({
@@ -38,6 +39,7 @@ import * as Joi from 'joi';
     }),
     ArticlesModule,
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
